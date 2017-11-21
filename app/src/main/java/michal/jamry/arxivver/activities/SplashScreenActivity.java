7cb3f4c1af4eca8f6ffd7ed0fcfa2c457e3857c5
michal jamry <jamrymi@gmail.com>
2017-11-22 00:28:49 +0100

    first

---
+++ app/src/main/java/michal/jamry/arxivver/activities/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package michal.jamry.arxivver.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import michal.jamry.arxivver.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
